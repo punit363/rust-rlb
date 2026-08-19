c2b574fe5b1f7096fc05a2683eb6be11b6525a59
Punit <3033.punit@gmail.com>
2026-08-20 02:11:32 +0530

    RLB : options enum and NULL

--- src/main.rs
+++ src/main.rs
@@ -19,4 +19,27 @@ impl Message{
 fn main(){
     let localhost = IpAddType::v4(127,0,0,1);
     Message::some_function();
+
+    //========================== Option Enum =========================
+    // Rust does not have the concept for null values
+    // If there is any encounter with a null value
+    // use Option Enum
+
+    enum Option<T>{
+        Some(T),
+        None
+    }
+
+    // Option enum is set in rust by default no need to create one like above
+
+    let x = 32;
+    let y = Some(48);
+
+    // let summation = x+y;
+    // above will throw error because y can be an int or cannot be
+
+    let sum = x + y.unwrap_or(0);
+
+    println!("sum: {}",sum);
+
 }
