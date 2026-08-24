48e8694cb53a7fd2981d5abfb7f810c82affd105
Punit <3033.punit@gmail.com>
2026-08-25 00:28:03 +0530

    RLB : lifetime ellistion

--- src/11_lifetimes/main.rs
+++ src/11_lifetimes/main.rs
@@ -1,14 +1,31 @@
 fn main() {
     let string1 = String::from("abcd");
 
-    let result;
     // this code generates error
+    // let result;
     // {
     //     let string2 = String::from("xyz");
     //     result = longest(string1.as_str(), string2.as_str());
     // }
 
     // println!("The longest sring is {}",result)
+
+    //Lifetime ellistion
+
+    //1. For a function each parameter that is used with reference gets its own lifetime parameter
+    //2. If there is a single input lifetime parameter, that lifetime parameter is assigned to all the output lifetime parmeters
+    //3. (only methods)If there are multiple input lifetime parameters but one of them is &self or &mut self the lifetime of that input is assigned to all the output parameters
+
+    struct ImportantExcerpt<'a> {
+        part:&'a str,
+    }
+
+    impl<'a> ImportantExcerpt<'a>{
+        fn return_part(&self,announcement:&str)->&str{
+            println!("announcement: {}",announcement);
+            self.part
+        }
+    }
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
